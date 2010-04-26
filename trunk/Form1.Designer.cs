83d4888499e1c1d6920d7b83b882d3ec015d83bf
guillaume.bittoun <guillaume.bittoun@c2443e7f-609e-b090-e4a1-8338d1bba20b>
2010-04-26 17:40:59 +0000

    Re-designing the MainFrame

--- trunk/Form1.Designer.cs
+++ trunk/Form1.Designer.cs
@@ -30,42 +30,65 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrame));
             this.editURL = new System.Windows.Forms.TextBox();
-            this.buttonDownload = new System.Windows.Forms.Button();
+            this.buttonChooseFolder = new System.Windows.Forms.Button();
             this.editPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // editURL
             // 
-            this.editURL.Location = new System.Drawing.Point(12, 12);
+            this.editURL.Location = new System.Drawing.Point(143, 12);
             this.editURL.Name = "editURL";
-            this.editURL.Size = new System.Drawing.Size(516, 20);
+            this.editURL.Size = new System.Drawing.Size(385, 20);
             this.editURL.TabIndex = 0;
             // 
-            // buttonDownload
+            // buttonChooseFolder
             // 
-            this.buttonDownload.Location = new System.Drawing.Point(12, 38);
-            this.buttonDownload.Name = "buttonDownload";
-            this.buttonDownload.Size = new System.Drawing.Size(516, 23);
-            this.buttonDownload.TabIndex = 1;
-            this.buttonDownload.Text = "Download pictures to...";
-            this.buttonDownload.UseVisualStyleBackColor = true;
-            this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
+            this.buttonChooseFolder.Location = new System.Drawing.Point(12, 38);
+            this.buttonChooseFolder.Name = "buttonChooseFolder";
+            this.buttonChooseFolder.Size = new System.Drawing.Size(110, 23);
+            this.buttonChooseFolder.TabIndex = 1;
+            this.buttonChooseFolder.Text = "Destination folder :";
+            this.buttonChooseFolder.UseVisualStyleBackColor = true;
+            this.buttonChooseFolder.Click += new System.EventHandler(this.buttonChooseFolder_Click);
             // 
             // editPath
             // 
-            this.editPath.Location = new System.Drawing.Point(12, 67);
+            this.editPath.Location = new System.Drawing.Point(143, 41);
             this.editPath.Name = "editPath";
             this.editPath.ReadOnly = true;
-            this.editPath.Size = new System.Drawing.Size(516, 20);
+            this.editPath.Size = new System.Drawing.Size(385, 20);
             this.editPath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Album RSS URL:";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 70);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(516, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "GO !";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
             // MainFrame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 97);
+            this.ClientSize = new System.Drawing.Size(540, 105);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.editPath);
-            this.Controls.Add(this.buttonDownload);
+            this.Controls.Add(this.buttonChooseFolder);
             this.Controls.Add(this.editURL);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -79,8 +102,10 @@
         #endregion
 
         private System.Windows.Forms.TextBox editURL;
-        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.Button buttonChooseFolder;
         private System.Windows.Forms.TextBox editPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStart;
     }
 }
 
